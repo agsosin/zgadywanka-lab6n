eb9554a170035e717246914fb8dbb551bc2546d7
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2019-05-18 19:31:32 +0200

    ModelGry

---
+++ GraZaDuzoZaMalo/ModelGry/Gra.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ModelGry
+{
+    public class Gra
+    {
+        // typy
+        public enum Odp { ZaMalo = -1, Trafiono = 0, ZaDuzo = +1 };
+        public enum StanGry { Trwa, Poddana, Odgadnieta };
+
+        // pola
+        private readonly int wylosowana;
+        public readonly int ZakresOd, ZakresDo;
+        public StanGry Stan { get; private set; }
+        public int LicznikRuchow { get; private set; }  = 0;
+        
+        //ToDo: historia rozgrywki
+
+        public Gra(int min, int max)
+        {
+            ZakresOd = min;
+            ZakresDo = max;
+            wylosowana = Losuj(ZakresOd, ZakresDo);
+            Stan = StanGry.Trwa;
+        }
+        private int Losuj(int min = 1, int max = 100)
+        {
+            Random generator = new Random();
+            int los = generator.Next(min, max + 1);
+            return los;
+        }
+
+        public Odp Ocena(int propozycja)
+        {
+            LicznikRuchow++;
+            if (propozycja < wylosowana)
+                return Odp.ZaMalo;
+            else if (propozycja > wylosowana)
+                return Odp.ZaDuzo;
+            else // ==
+            {
+                Stan = StanGry.Odgadnieta;
+                return Odp.Trafiono;
+            }
+        }
+
+        public void Poddaj()
+        {
+            Stan = StanGry.Poddana;
+        }
+
+        public int? CoByloWylosowane()
+        {
+            if( Stan != StanGry.Trwa )
+                return wylosowana;
+
+            return null;
+        }
+
+    }
+}
